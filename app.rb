11060b5b9b687d466a890ea77d5a5009b249bc68
Gabriel Pereira <pereira@Gabriels-MacBook-Pro.local>
2016-09-21 15:02:20 -0400

    List all PRs with its ninja check status

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'byebug'
 require 'rack/parser'
 require 'dotenv'
 require './github_webhook'
+require 'pry'
+require './github'
 
 module NinjaSeal
   class App < Sinatra::Base
@@ -11,10 +13,24 @@ module NinjaSeal
       'application/json' => -> (data) { JSON.parse(data) }
     }
 
-    use GithubWebhook
+    set :views, 'views'
 
-    post '/webhooks/tracker' do
-      'Put this in your pipe & smoke it!'
+    get '/pullrequests' do
+      pr_resource = Octokit.pull_requests('powerhome/nitro-web', status: 'open')
+      pr_list = []
+      pr_resource.each do |pr|
+        statuses = Octokit.combined_status('powerhome/nitro-web', pr[:head][:sha])[:statuses]
+        status = statuses.select do |st|
+          st[:context] == 'ninja-seal'
+        end
+
+        if !status.empty?
+          pr_list << {title: pr[:title], status: status.state }
+        else
+          pr_list << {title: pr[:title], status: 'none' }
+        end
+      end
+      erb :pullrequests, locals: {prs: pr_list}
     end
 
     run!
